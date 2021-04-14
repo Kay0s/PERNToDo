9d70d29e17adfd1512da5594fbce215a7d3f878e
Kay0s <hamilton.kristina@gmail.com>
2021-04-13 20:35:20 -0500

    added post routes to index.js

---
+++ server/db.js
@@ -0,0 +1,15 @@
+const Pool = require("pg").Pool;
+require("dotenv").config();
+const { PGDATABASE, PGPASSWORD, PGHOST, PGPORT, PGUSER   } = proces.env;
+
+
+const pool = new Pool ({
+    user: PGUSER,
+    password: PGPASSWORD,
+    host: PGHOST,
+    port: PGPORT,
+    database: PGDATABASE
+
+});
+
+module.exports = pool; 
--- server/index.js
+++ server/index.js
@@ -1,10 +1,37 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+require("dotenv").config();
+const pool = require("./db");
+
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+//Routes//
+
+//create a todo
+app.post("/todos", async(req,res) => {
+    try {
+       const { description } = req.body;
+       const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1) RETURNINg *", 
+       [description]
+       );
+
+       res.json(newTodo);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+//get all todos
+
+//get a todo
+
+//update a todo
+
+//delete a todo
 
 app.listen(5000, () => {
     console.log("server has started on port 5000")
