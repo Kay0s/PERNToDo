ea37bd8310d45ff75675a39374998280c32c6cbe
Kay0s <hamilton.kristina@gmail.com>
2021-04-13 20:38:36 -0500

    updated RETURNING on index.js

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.post("/todos", async(req,res) => {
     try {
        const { description } = req.body;
-       const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1) RETURNINg *", 
+       const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1) RETURNING *", 
        [description]
        );
 
