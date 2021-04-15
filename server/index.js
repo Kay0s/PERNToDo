66509deea63d8bfae55e17ef080dd47d38e986de
Kay0s <hamilton.kristina@gmail.com>
2021-04-14 21:21:25 -0500

    added app.get code block to server.index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 //Routes//
 
 //create a todo
-app.post("/todos", async(req,res) => {
+app.post("/todos", async (req,res) => {
     try {
        const { description } = req.body;
        const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1) RETURNING *", 
@@ -20,12 +20,20 @@ app.post("/todos", async(req,res) => {
        );
 
        res.json(newTodo.rows[0]);
-    } catch (err) {
-        console.error(err.message);
+    }  catch (err) {
+       console.error(err.message);
     }
 });
 
 //get all todos
+app.get("/todos", async (req,res) => {
+    try {
+       const allTodos = await pool.query("SELECT * FROM todo");
+       res.json(allTodos.rows);
+    } catch (err) {
+      console.error(err.message);
+    }
+});
 
 //get a todo
 
