116e38c59cc9c96f0405dfca5d90eb19043583c4
Kay0s <hamilton.kristina@gmail.com>
2021-04-16 23:10:11 -0500

    useState code established

--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -1,11 +1,17 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
 const InputTodo = () => {
+
+    const [description, setDescription ] = useState("");
+
     return (
       <>
       <h1 className="text-center mt-5">PERN Todo List</h1>
       <form className="d-flex mt-5">
-          <input type="text" className="form-control" />
+          <input type="text" 
+          className="form-control" 
+          value={description} 
+          onChange={e => setDescription(e.target.value)}/>
           <button className="btn btn-success">Add</button>
       </form>
      </>
