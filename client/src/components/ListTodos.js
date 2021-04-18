2aca017e7392042fcba7699b6f49a66dea1d6524
Kay0s <hamilton.kristina@gmail.com>
2021-04-18 18:01:59 -0500

    updated set error on ListTodos.js

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -1,15 +1,15 @@
 import React, { Fragment, useEffect, useState } from "react";
 
-import EditTodo from "./EditTodo";
+// import EditTodo from "./EditTodo";
 
-const ListTodos = () => {
-  const [todos, setTodos] = useState([]);
+ const ListTodos = () => {
+ const [todos, setTodos] = useState([]);
 
-  //delete todo function
+//delete todo function
 
-  const deleteTodo = async id => {
+  async function deleteTodo(id) {
     try {
-      const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {
+       const res = await fetch(`http://localhost:5000/todos/${id}`, {
         method: "DELETE"
       });
 
@@ -19,16 +19,15 @@ const ListTodos = () => {
     }
   };
 
-  const getTodos = async () => {
-    try {
-      const response = await fetch("http://localhost:5000/todos");
-      const jsonData = await response.json();
+ async function getTodos () {
+      const res = await fetch("http://localhost:5000/todos");
 
-      setTodos(jsonData);
-    } catch (err) {
-      console.error(err.message);
-    }
-  };
+      const  todoArray = await res.json();
+
+      setTodos(todoArray);
+
+}
+
 
   useEffect(() => {
     getTodos();
@@ -38,7 +37,7 @@ const ListTodos = () => {
 
   return (
     <Fragment>
-      {" "}
+       {" "}
       <table class="table mt-5 text-center">
         <thead>
           <tr>
@@ -51,23 +50,20 @@ const ListTodos = () => {
           {todos.map(todo => (
             <tr key={todo.todo_id}>
               <td>{todo.description}</td>
-              <td>
-                <EditTodo todo={todo} />
-              </td>
-              <td>
-                <button
-                  className="btn btn-danger"
-                  onClick={() => deleteTodo(todo.todo_id)}
-                >
-                  Delete
+              <td>Edit</td>
+              <button className="btn 
+              btn-danger" onClick={() => 
+              deleteTodo(todo.todo_id)}
+              >
+                Delete
                 </button>
-              </td>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          ))} 
+        </tbody> 
+      </table> 
     </Fragment>
   );
-};
+ };
 
 export default ListTodos;
+
